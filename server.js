429d1a7b47d8606d0d10233e8eb95d5e370e860e
Alexandre Cesar <acmorcego@gmail.com>
2019-08-11 19:44:56 -0300

    extract getWords

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require('express')
 const Telegram = require('telegraf/telegram')
 const seedrandom = require('seedrandom')
 
-const english = require('./data/english.json')
-
 const token = process.env.SECRET
 const telegram = new Telegram(token)
 const send = (chat, message) =>
@@ -13,21 +11,34 @@ const send = (chat, message) =>
     ? console.log({ chat, message })
     : telegram.sendMessage(chat, message)
 
-const rng = seedrandom(new Date().toDateString())
-const words = new Array(10)
-  .fill(0)
-  .map(() => english[Math.floor(english.length * rng())])
-  .map(word => `- ${word}`)
-  .join('\n')
+function getWords(language) {
+  const data = require(`./data/${language}.json`)
+  const rng = seedrandom(new Date().toDateString())
+  const words = new Array(10)
+    .fill(0)
+    .map(() => data[Math.floor(data.length * rng())])
+    .map(word => `- ${word}`)
+    .join('\n')
+  return words
+}
 
 const app = express()
 app.use(express.json())
 app.post(`/${token}`, (request, response) => {
   const { message } = request.body
-  if (message && message.text && message.text === '/en')
-    send(message.chat.id, words)
+  if (message && message.text && message.text.startsWith('/')) {
+    switch (message.text) {
+      case '/en':
+      case '/english':
+        send(message.chat.id, getWords('english'))
+        break
+      default:
+        send(message.chat.id, 'unknown command')
+    }
+  }
   response.send('')
 })
+
 const listener = app.listen(process.env.PORT, () =>
   console.log('Your app is listening on port ' + listener.address().port)
 )
